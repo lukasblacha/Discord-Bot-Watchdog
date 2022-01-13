6fdfcb9fb0e67a83b1ac5e002683cd8f10e517b5
Lukas Blacha <lukas.blacha@rub.de>
2022-01-13 13:06:56 +0100

    Removed commands - Added settings in config-File

--- WatchBot.py
+++ WatchBot.py
@@ -1,12 +1,5 @@
 import discord
-import slash as slash
 from discord.ext import commands
-from discord.utils import get
-from discord_slash import SlashCommand, ComponentContext
-from discord_slash.utils.manage_commands import create_choice, create_option, create_permission
-from discord_slash.utils.manage_components import create_actionrow, create_button, wait_for_component, create_select, \
-    create_select_option
-from discord_slash.model import ButtonStyle, SlashCommandPermissionType
 import configparser
 import os
 import datetime
@@ -18,22 +11,31 @@ print(f'# Version {version}            #')
 print('##########################')
 print('\n')
 
+CONFIG_DIR = "config"
 DB_FILE = "./config/DATABASE.db"
-CONFIG_FILE = "config/settings.ini"
+CONFIG_FILE = "config/config.ini"
 config = configparser.ConfigParser()
 
 ##### CONFIG HANDELING #####
-if os.path.exists(CONFIG_FILE):
-    pass
-else:
-    print("Creating config-file \n")
+if not os.path.exists(CONFIG_DIR):
+    print("Creating CONFIG_DIR\n")
+    os.system(f"mkdir {CONFIG_DIR}")
+if not os.path.exists(CONFIG_FILE):
+    print("Creating CONFIG_FILE \n")
     config.add_section("Settings")
     config.set("Settings", "TOKEN", "")
     config.set("Settings", "BOT_PREFIX", "!")
-    config.set("Settings", "OWNER_ID", "")
+    config.set("Settings", "OWNER_ID", "150933142482452480")
+    config.add_section("Subscribers")
+    config.set("Subscribers", "USERs", "")
+    config.add_section("Observed")
+    config.set("Observed", "BOTs", "")
     file = open(CONFIG_FILE, 'w')
     config.write(file)
     file.close()
+else:
+    print("All Direcories and Files in place!\nContinuing")
+
 
 config.read(CONFIG_FILE)
 BOT_PREFIX = config.get('Settings', 'BOT_PREFIX')
@@ -45,13 +47,14 @@ else:
     TOKEN = config.get("Settings", "TOKEN")
     BOT_PREFIX = config.get("Settings", "BOT_PREFIX")
     OWNER_ID = config.get("Settings", "OWNER_ID")
+    USERS = config.get("Subscribers", "USERs").split()
+    BOTS = config.get("Observed", "BOTs").split()
 
 ##### COLORS #####
 RED = 0xe91e63
 GREEN = 0x2ecc71
 
 class NOTIFY:
-    #User mention with <@ID>, Role mention with <@&ID>
     async def notify_user(user_id: int, title: str, name: str, content: str, colour: int, thumbnail: str):
         user = client.get_user(int(user_id))
         embed = discord.Embed(title=title, colour=colour)
@@ -71,7 +74,7 @@ class NOTIFY:
 if __name__ == '__main__':
     client = commands.Bot(command_prefix=BOT_PREFIX, intents=discord.Intents.all(), case_insensitive=True,
                           owner_id=OWNER_ID)
-    slash = SlashCommand(client, sync_commands=True)
+    #slash = SlashCommand(client, sync_commands=True)
 
 
     @client.event
@@ -85,53 +88,31 @@ if __name__ == '__main__':
             f"https://discord.com/api/oauth2/authorize?client_id={client.user.id}&permissions=261993005047&scope=bot%20applications.commands")
         print("--------------------------------------------------")
         game = discord.Game("/help")
-        await client.change_presence(status=discord.Status.online, activity=game)
+        await client.change_presence(status=discord.Status.offline)
 
     @client.event
     async def on_member_update(before, after):
-        print("MEMBER UPDATE")
-        print(f"Status-ÄNDERUNG \nUser: {before} Status: {before.status} --> User: {after} Status: {after.status}")
-        print(f"User is Bot: {before.bot}")
-        if before.id == 411798936643436545:
-            if str(after.status) == "offline":
-                await NOTIFY.notify_user(150933142482452480, "‼️ STATUS NOTIFICATION ‼️", f"User: {after}",
-                                         f"**Status: *{after.status}***",
-                                         RED, before.avatar_url)
-            else:
-                await NOTIFY.notify_user(150933142482452480, "🔔 STATUS NOTIFICATION 🔔", f"User: {after}",
-                                         f"**Status: *{after.status}***",
-                                         GREEN, before.avatar_url)
-        else:
-            pass
+        if before.bot == True:
+            print("MEMBER UPDATE")
+            print(f"{datetime.datetime.utcnow()} - Status-ÄNDERUNG \nUser: {before} Status: {before.status} --> User: {after} Status: {after.status}")
+            for filter_user in BOTS:
+                if int(filter_user) == before.id:
+                    if str(after.status) == "offline":
+                        for i in USERS:
+                            await NOTIFY.notify_user(int(i), "‼️ STATUS NOTIFICATION ‼️", f"User: {after}",
+                                                    f"**Status: *{after.status}***", RED, before.avatar_url)
+                    elif str(after.status) == "online":
+                        pass
+                        """
+                        await NOTIFY.notify_user(150933142482452480, "🔔 STATUS NOTIFICATION 🔔", f"User: {after}",
+                                                 f"**Status: *{after.status}***",
+                                                 GREEN, before.avatar_url)
+                        """
 
-
-    @slash.slash(name="watchbot", description="Shows the available WatchBot commands", options=[
-        create_option(
-            name="obervation",
-            description="Gib deinem Team eine Farbe",
-            option_type=1,
-            required=False,
-            options=[
-                create_option(
-                    name="watch",
-                    description="Gib deinem Team eine Farbe",
-                    option_type=6,
-                    required=True),
-                create_option(
-                    name="unwatch",
-                    description="Gib deinem Team eine Farbe",
-                    option_type=6,
-                    required=True)
-            ]
-        )
-    ]
-                 )
-    async def _watchbot(ctx, watch, unwatch):
-        await ctx.send("TEST Message", hidden=True)
-        if watch != None:
-            print(watch)
-        elif unwatch != None:
-            print(unwatch)
+                    else:
+                        pass
+                else:
+                    pass
         else:
             pass
 
